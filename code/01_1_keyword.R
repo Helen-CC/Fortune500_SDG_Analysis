56e64dfedd0a099ad5569a3aa203f043d45a9528
a0981906660 <a0981906660@gmail.com>
2022-09-07 23:58:14 -0500

    renamed file

---
+++ code/01_1_keyword.R
@@ -0,0 +1,113 @@
+rm(list = ls())
+# Load necessary packages
+library(tidyverse)
+library(readxl)
+# library(clipr)
+library(hash)
+
+# Setup working directory
+setwd("/Volumes/TOSHIBA3T/2022-09-07/Fortune500_SDG_Analysis")
+getwd()
+
+# Load keyword dictionary
+df_keyword <- readxl::read_excel("./data/raw_data/UoA-SDG-Keyword-List-Ver.-1.1.xlsx", 
+                                 sheet = "keyword") %>% 
+  rename(word = `SDG Keywords`, word2 = `Alternatives`, sdg = `SDG`)
+
+# Combine the 2 colums -> use keywords in both columns at once
+df_keyword_unnest <- df_keyword %>% 
+  select(sdg, word) %>% 
+  bind_rows(df_keyword %>% 
+              select(word2, sdg) %>% 
+              drop_na() %>% 
+              rename(word = word2)
+            ) %>% 
+  # sort the rows in SDG order
+  mutate(SDG_order = str_extract(sdg, pattern = "\\d+"),
+         SDG_order = as.numeric(SDG_order),
+         ) %>% 
+  arrange(SDG_order) %>% 
+  select(-SDG_order) %>% 
+  # trim white spaces at both sides
+  mutate(word = str_trim(word, side = "both")) %>% 
+  # add regular expressions
+  mutate(word = str_replace_all(word, "\\*", ".?")) %>%
+  mutate(word = str_replace_all(word, " AND ", ".*?")) %>% #舉例 Economic Resource AND Access 在一個句子裡面同時出現，不一定要前後
+  mutate(word = str_split(word, "; ")) %>% #把excel 裡面同一格有 分號; 的分開到不同row 如row 101
+  unnest(c(word))
+
+# Create a dataframe of keywords without spaces -> nspace
+df_keyword_nspace <- df_keyword_unnest %>% 
+  filter(!str_detect(word, " ")) %>% # TODO: better change space into `\\s`
+  ## TODO: need to know the variable meaning
+  mutate(lll = str_length(word), lll_b = str_count(word, "[A-Z]")) %>%
+  mutate(word = if_else(lll > lll_b, str_to_lower(word), word))
+# Create a dataframe of keywords with spaces
+df_keyword_space <- df_keyword_unnest %>% 
+  filter(str_detect(word," ")) %>%
+  mutate(word = str_to_lower(word))
+
+# ## The following code demonstrate that why `\s` is better than a single white space
+# df.test.s <- df_keyword_unnest %>% 
+#   filter(!str_detect(word, "\\s")) %>% 
+#   mutate(lll = str_length(word), lll_b = str_count(word, "[A-Z]")) %>%
+#   mutate(word = if_else(lll > lll_b, str_to_lower(word), word))
+# df.test.space <- df_keyword_unnest %>% 
+#   filter(!str_detect(word, " ")) %>% 
+#   mutate(lll = str_length(word), lll_b = str_count(word, "[A-Z]")) %>%
+#   mutate(word = if_else(lll > lll_b, str_to_lower(word), word))
+# x <- df.test.s$word
+# y <- df.test.space$word
+# setdiff(y, x)
+
+
+# Create a dataframe to merge the dataframe with and without spaces
+df_bind <- df_keyword_nspace %>% 
+  bind_rows(df_keyword_space) %>%
+  mutate(word = str_trim(word, "both")) %>%
+  mutate(word = if_else(str_detect(word, "\\.\\*\\?"), 
+                        word, str_c("\\b",word,"\\b"))) %>%
+  # sort the rows in SDG order
+  mutate(SDG_order = str_extract(sdg, pattern = "\\d+"),
+         SDG_order = as.numeric(SDG_order),
+         ) %>% 
+  arrange(SDG_order) %>% 
+  select(sdg, word) 
+
+# clean up env
+rm(list=setdiff(ls(), "df_bind"))
+
+
+# Load the manual edited keyword mapping
+# https://docs.google.com/spreadsheets/d/1fZdE9WcFYI_d_sD4BgBpI5D1QhOYlRngsuSEtB7w694/edit#gid=470532546
+df_manual <- read_excel("./data/raw_data/manual_edit_keywords.xlsx",
+                        sheet = "df_manual")
+h <- hash(keys = df_manual$word, values = df_manual$word_new)
+# e.g. 
+# h[[df_manual$word[1]]]
+mapKeyVal <- function(keys) {
+  res <- c()
+  for (key in keys) {
+    res <- c(res, h[[key]])
+  }
+  return(res)
+}
+# df_manual$word[1:3] %>% mapKeyVal()
+
+# replace the original keywords
+df_bind <- df_bind %>% 
+  mutate(word = ifelse(word %in% df_manual$word,
+                        mapKeyVal(word),
+                        word))
+# clean up
+df_final_key <- df_bind %>%
+  mutate(word = str_replace_all(word, "\\{", "(")) %>%
+  mutate(word = str_replace_all(word, "\\}", ")")) %>%
+  mutate(word = str_remove_all(word, "“|”")) %>%
+  filter(!str_detect(word, "not")) %>%
+  distinct() %>%
+  filter(!str_detect(word,"goverance")) #看看要不要governance 這個字
+
+# Save file
+df_final_key %>% 
+  write_rds("./data/cleaned_data/df_final_key.rds")
