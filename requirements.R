c596fa2b5f256480167d05aa73e750b3aeae3b00
a0981906660 <a0981906660@gmail.com>
2022-09-20 22:51:56 -0500

    added requirements and pkg installer

---
+++ requirements.R
@@ -0,0 +1,32 @@
+pkgLoad <- function( packages = "requirements" ) {
+  
+  if( length( packages ) == 1L && packages == "requirements" ) {
+    # packages <- c( "data.table", "chron", "plyr", "dplyr", "shiny",
+    #                "shinyjs", "parallel", "devtools", "doMC", "utils",
+    #                "stats", "microbenchmark", "ggplot2", "readxl",
+    #                "feather", "googlesheets", "readr", "DT", "knitr",
+    #                "rmarkdown", "Rcpp")
+    packages <- c( "tidyverse", "tidytext", "fs", "stringi", "readxl",
+                   "hash")
+  }
+  
+  packagecheck <- match( packages, utils::installed.packages()[,1] )
+  
+  packagestoinstall <- packages[ is.na( packagecheck ) ]
+  
+  if( length( packagestoinstall ) > 0L ) {
+    utils::install.packages( packagestoinstall,
+                             repos = "https://cloud.r-project.org"
+    )
+  } else {
+    print( "All requested packages already installed" )
+  }
+  
+  for( package in packages ) {
+    suppressPackageStartupMessages(
+      library( package, character.only = TRUE, quietly = TRUE )
+    )
+  }
+  
+}
+pkgLoad()
