eb763da6d861f609c469ad559c88c112cb1de36d
a0981906660 <a0981906660@gmail.com>
2022-09-25 15:58:03 -0500

    finished cleanning the third script

--- code/03_1_TF-IDF.R
+++ code/03_1_TF-IDF.R
@@ -1,186 +1,106 @@
-install.packages("viridis")
+rm(list = ls())
 library(tidyverse)
 library(tidytext)
 library(ggrepel)
 library(viridis)
+library(ggplot2)
+source("./code/weakWords.R")
 
-df_final_key <- read_rds("df_final_key.rds")
+df_final_key <- read_rds("./data/cleaned_data/df_final_key.rds")
 
 # IO of annual reports
-foldername <- list.files(path = "F:\\Fortune  500 report", pattern = '', full.names = T)
-# make sure they are all folders(資料夾)
-foldername <- foldername[!str_detect(foldername, "\\.xlsx$|\\.docx$|\\.R$|\\.ini$")]
-foldername <- foldername[!str_detect(foldername, "udpipe")]
-filename <- list()
-i <- 1
-for (i in 1:length(foldername)) {
-  print(i)
-  inner_foldername_tmp <- list.files(path = foldername[i], pattern = '', full.names = T)
-  inner_foldername <- inner_foldername_tmp[!str_detect(inner_foldername_tmp, "pdf")]
-  filename[[i]] <- list.files (path= inner_foldername, pattern = "", full.names = T)
-}
+txt_files <- fs::dir_ls("./data/raw_data/Fortune_500_report/", 
+                        recurse = TRUE, regexp = "\\.txt")
+df.txt <- bind_cols(
+  path = txt_files
+  ) %>% 
+  mutate(rank = str_replace(path, "./data/raw_data/Fortune_500_report/", "")) %>% 
+  mutate(rank = str_extract(rank, pattern = "\\d+"))
 
-index_now <- 1
-k <- index_now
+# Select txt files based on NAICS code
+## Load company Rank and NAICS code mapping
+df.RankCode <- readxl::read_excel("./data/raw_data/TM Final_FortuneG500 (2021)_v2.xlsx", 
+                                  sheet = "Fortune Global 500 2021") %>% 
+  select(rank = Rank, 
+         name = Name, 
+         sic = `SIC Code`, 
+         naics = `NAICS Code & Description (Eikon)`) %>% 
+  mutate(naics2 = floor(naics/100))
 
-### 以公司為單位的 tf idf
-### 這邊在外面沒錯 要重跑就要歸零
+# A BETTER WAY TO SELECT NAICS STARTING WITH 31
+## TODO: input the NAICS2 you want
+NAICS2 <- 21
+TARGET_ROWS <- df.RankCode %>% 
+  filter(naics2 == NAICS2) %>% 
+  pull(rank) %>% 
+  unique() %>% 
+  sort()
+
+# compute word count
+## After selecting specific companies
+txt.toRead <- df.txt %>% 
+  filter(rank %in% TARGET_ROWS) %>% 
+  pull(path)
+
+## new a df to store the path of the file to read and the contents
 df_doc <- tibble()
-### 讀你要的 text
-for (k in c(445, 46, 484, 18, 30, 107, 127, 151, 161, 164, 392, 268, 469, 136, 339, 425, 194, 217, 333, 343, 175, 307)) {
-  for (fn in 1:length(filename[[k]])) {
-    df_doc_tmp <- read_lines(filename[[k]][fn]) %>% as_tibble() %>% 
-      summarise(value = str_c(value, collapse = " ")) %>%
-      mutate(name = filename[[k]][fn])
-    df_doc <- df_doc %>% bind_rows(df_doc_tmp)
-  }
-  message("finish", "")
-  index_now <- index_now + 1
-  #Sys.sleep(60)
+for (txt in txt.toRead) {
+  filename <- str_replace(txt, "./data/raw_data/Fortune_500_report/", "") %>% 
+    str_extract("\\d+(\\s\\w+)+")
+  year <- str_extract(txt, "20\\d{2}")
+  df.tmp <- read_lines(txt) %>% 
+    as_tibble() %>% 
+    summarise(value = str_c(value, collapse = "\\s")) %>% 
+    mutate(name = filename,
+           rank = str_extract(name, "\\d+"),
+           rank = as.numeric(rank),
+           year = as.numeric(year)) %>% 
+    drop_na() %>% 
+    arrange(rank, year)
+  df_doc <- df_doc %>% bind_rows(df.tmp)
 }
 
-# Resourcing  global growth Annual Report  2015
-df_word <- df_doc %>% 
-  unnest_ngrams(output = word, input = value, n = 2, n_min = 1, ngram_delim = "___", stopwords = stopwords::stopwords())
 
-df_word_uni <- df_word %>% filter(!str_detect(word, "[0-9]")) %>% 
-  filter(!str_detect(word, "___")) %>%
-  filter(word!="aramco") %>% #可以放tfidf 已出現但不想要的字，不用正規表達，一次放一個字
-  filter(word!="sinopec") %>%
-  filter(word!="glencore") %>%
-  filter(word!="gazprom") %>%
-  filter(word!="sar") %>%
-  filter(word!="h.e") %>%
-  filter(word!="koz") %>%
-  filter(word!="ffo") %>%
-  filter(word!="indd") %>%
-  filter(word!="chen") %>%
-  filter(word!="ooo") %>%
-  filter(word!="neft") %>% 
-  filter(word!="bcm") %>%
-  filter(word!="pao") %>%
-  filter(word!="hsec") %>%
-  filter(!(word %in% c("fy","ovl","x", "m", "anglo", "bhp", "bhp's", "equinor",
-                       "", "rquinor\\'s", "indianoil", "nn", "shri", "ncnn", "nc",
-                       "nnnn", "nnc", "ncn", "lukoil", "company\\'s", "mitsui", "mitsui\\'s",
-                       "ihh", "div", "ind", "tinto", "riotinto.com", "rio", "tinto\\'s",
-                       "rosneft", "rosneft's", "x", "rn", "company's", "fi", "saudi", "group's",
-                       "leung", "vale", "ore", "refi", "dan", "crore", "cnn", "nnn", "nnno",
-                       "cerrej", "nok", "hkfrs", "soes", "tahun", "pada", "kumba", "waio", "dlc",
-                       "carlton", "cutifani", "ordos", "dpi", "asa", "ugsfs", "ngv", "yamal",
-                       "oao", "cerrej", "ompany", "ndian", "contd", "qurna", "ipp",
-                       "gestamp", "penske", "ongc", "hpcl", "goi", "sez", "tolgoi",
-                       "oyu", "stip", "kennecott", "pjsc", "mmt", "krub", "refi", "riyals",
-                       "rabigh", "zakat", "arlanxeo", "hkfrs", "henan", "hkfrss", "yuk",
-                       "hainan", "dan", "pada", "untuk", "dari","quellaveco", "namdeb", "datun",
-                       "ncs", "sdfi", "ugsf", "ao", "neftekhim", "kt", "cn", "nncn", "rore", "ritek",
-                       "timan", "bussan", "ptl", "videsh", "ompl", "ccps", "upto", "jean", "csc",
-                       "6o", "jsc", "yuganskneftegaz", "mmb", "shpp", "scfd", "pif", "deruyck", "epec",
-                       "perseroan", "dengan", "dalam", "komisaris", "dewan", "atau", "keuangan",
-                       "direksi", "〇", "бо", "х"))) %>% 
-  filter(!str_detect(word, "mitsui.?s|group.?s|statoil.?"))#’‘
-df_word_uni %>% count(name)
+### TF-IDF for the selected firms 
 
-df_word_bi <- df_word %>% filter(!str_detect(word, "[0-9]")) %>%
-  filter(str_detect(word, "___"))
+# create tokens (words) from sentences
+df.tokens <- df_doc %>% unnest_tokens(word, value) %>% 
+  filter(!word %in% stopwords::stopwords()) %>% 
+  filter(!word %in% base::letters) %>% 
+  anti_join(stop_words) %>% 
+  filter(!str_detect(word, "\\d+\\w")) %>% 
+  filter(!str_detect(word, "\\d+")) %>% 
+  filter(!word %in% weak_words)
 
-df_word_uni_n <- df_word_uni %>% 
-  #mutate(name = str_remove(name, " text file/.*|txt/.* |Group/.*")) %>%
-  mutate(name = str_remove_all(name, "([0-9]){1,4}.txt|F:\\Fortune  500 report/"))%>%
-  count(name, word)
-df_word_uni_n %>% count(name)
+# regardless of year
+df.words <- df.tokens %>%
+  count(name, word, sort = TRUE)
 
-df_uni_tfidf <- df_word_uni_n %>% 
-  anti_join(stop_words) %>%
-  bind_tf_idf(word, name, n)
+total.words <- df.words %>% group_by(name) %>% summarize(total = sum(n))
+df.words <- left_join(df.words, total.words, by = "name")
 
-df_word_uni_n %>% write_rds("df_word_uni_n_21.rds") #目前結果存出去
-df_uni_tfidf %>% write_rds("df_uni_tfidf21.rds")
-df_uni_tfidf %>% write_csv("df_uni_tfidf21.csv")
 
-#tfidf 畫圖
+# TF-IDF
+df.words <- df.words %>%
+  bind_tf_idf(word, name, n) %>% 
+  arrange(desc(tf_idf))
 
-df_uni_tfidf %>% count(name)
+df.words <- df.words %>% 
+  mutate(name = factor(name, levels = unique(df.words$name)))
 
-#df_uni_tfidf %>% mutate(name = str_remove_all(name, "F:\\Fortune  500 report/"))
-df_uni_tfidf_cleanname <- df_uni_tfidf %>%
-              mutate(name = str_remove_all(name, "F:\\\\Fortune  500 report/"))%>%
-              mutate(name = str_remove_all(name, "/.*"))%>%
-              mutate(name = str_remove_all(name, "[0-9]{1,4} "))
-df_uni_tfidf_cleanname %>% count(name)
-  
-p_tw_black <-
-df_uni_tfidf_cleanname %>% #filter(name %in% c("Anglo American",
-                                              #"BHP Group",
-                                              #"China National Coal Group",
-                                              #"Equinor",
-                                              #"Gazprom",
-                                              #"Glencore",
-                                              #"Indian Oil",
-                                              #"Lukoil"))
-  group_by(name) %>% #我在網路上查的
-  top_n(10) %>% 
-  ungroup() %>% view 
-  #mutate(name = if_else(str_detect(name, "Natural Gas"), "Indian Oil & Natural Gas", name)) %>%
-  mutate(name = if_else(str_detect(name, "Yanchang"), "Shaanxi Yanchang Petroleum", name))  %>%
-  mutate(name = as.factor(name),
-         word = reorder_within(word, tf_idf, name)) %>% #name 公司名，tf_idf 
-  #mutate(word = reorder(word, tf_idf)) %>% 
-  ggplot(aes(word, tf_idf, fill = name)) +
+p1 <- df.words %>% 
+  group_by(name) %>% 
+  slice_max(tf_idf, n = 10) %>% 
+  ungroup() %>%
+  mutate(word = reorder(word, tf_idf)) %>%
+  ggplot(aes(tf_idf, word, fill = name)) +
   geom_col(show.legend = FALSE) +
-  facet_wrap(~name, scales = "free") +
-  coord_flip() +
-  scale_x_reordered() +
-  scale_y_continuous(expand = c(0,0)) +
-  labs(y = "TF-IDF",
-       x = NULL,
-       title = "What were the most common baby names in each decade?",
-       subtitle = "Via US Social Security Administration") 
-  #scale_fill_manual(values=c("blue", "black", "gold", "red")) #自己選顏色
-  #scale_fill_viridis(option="plasma", discrete = TRUE)
-  
-p_tw_black %>% 
-  ggsave(filename = "tfidf21.png", dpi = 300, height = 12, width = 12)
+  labs(x = "TF-IDF", y = NULL) +
+  facet_wrap(~name, ncol = 2, scales = "free")
 
-#子軒原來寫的
-group_by(name) %>% arrange(-tf_idf) %>%
-  slice(1:15) %>% ungroup() %>%
-  ggplot(aes(x = word, y = tf_idf)) + geom_col() +
-  facet_wrap(name ~ ., scale = "free") +
-  coord_flip()
-
-#bi_n 算跟作圖
-df_word_bi_n <- df_word_bi %>% 
-  mutate(name = str_remove(name, " text file/.*")) %>%
-  count(name, word)
-
-df_bi_tfidf <- df_word_bi_n %>% 
-  anti_join(stop_words) %>%
-  bind_tf_idf(word, name, n)
-
-#113-124 網上查錯Faceting variables must have at least one value
-df_bi_tfidf %>% filter(name %in% c("F:\\Fortune  500 report/5 Sinopec Group/Sinopec Group AR txt/Sinopec Group AR ",
-                                    "F:\\Fortune  500 report/14 Saudi Aramco/Saudi Aramco AR txt/Saudi Aramco AR ",
-                                    "F:\\Fortune  500 report/34 Glencore/Glencore AR txt/Glencore AR ",
-                                    "F:\\Fortune  500 report/84 Gazprom/Gazprom AR txt/Gazprom AR ")) %>%
-  group_by(name) %>%
-  top_n(10) %>% 
-  ungroup() %>% 
-  mutate(word = reorder(word, tf_idf)) %>% 
-  ggplot(aes(word, tf_idf, fill = name)) +
-  geom_col(show.legend = FALSE) +
-  facet_wrap(~name, scales = "free") +
-  coord_flip()
-#126-134 子軒寫錯Faceting variables must have at least one value
-df_bi_tfidf %>% filter(name %in% c("F:\\Fortune  500 report/5 Sinopec Group/Sinopec Group AR txt/Sinopec Group AR ",
-                                   "F:\\Fortune  500 report/14 Saudi Aramco/Saudi Aramco AR txt/Saudi Aramco AR ",
-                                   "F:\\Fortune  500 report/34 Glencore/Glencore AR txt/Glencore AR ",
-                                   "F:\\Fortune  500 report/84 Gazprom/Gazprom AR txt/Gazprom AR ")) %>%
-  group_by(name) %>% arrange(-tf_idf) %>% 
-  slice(1:10) %>% ungroup() %>%
-  ggplot(aes(x = word, y = tf_idf)) + geom_col() +
-  facet_wrap(name ~ ., scale = "free") +
-  coord_flip()
-
-df_word_bi_n %>% write_rds("data/df_word_bi_n.rds")
-df_bi_tfidf %>% write_rds("data/df_bi_tfidf.rds")
+p1 %>% 
+  ggsave(filename = "./data/result/fig_Tf-IDF_NAICS_21.png", 
+         device = "png",
+         dpi = 300, 
+         units = "in",
+         height = 12, width = 12)
