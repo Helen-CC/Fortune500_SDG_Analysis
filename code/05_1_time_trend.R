420acb33d6d24e30710211baed483baeeb2e543a
a0981906660 <a0981906660@gmail.com>
2022-10-08 21:20:09 -0500

    added time trend that a company mentioned each SDG categories

--- code/05_1_time_trend.R
+++ code/05_1_time_trend.R
@@ -1,3 +1,4 @@
+rm(list = ls())
 # 05
 # time series line plot showing the ratio of keyword presence over years and across SDG categories
 # ratio = number of keywords shown / total number of words
@@ -5,3 +6,68 @@
 # the x-axis is time (year)
 # the y-axis is ratio
 # SDG0-SDG16 -> 17 lines
+
+
+library(dplyr)
+library(ggplot2)
+library(readr)
+library(stringr)
+
+# Load data
+df <- read_rds("./data/cleaned_data/df_wordCount_naics_31.rds")
+df_final_key <- read_rds("./data/cleaned_data/df_final_key.rds")
+
+# Merge SDG categories
+df <- df %>% 
+  left_join(df_final_key, by = c('keyword' = 'word')) %>% 
+  # add year and rank
+  mutate(year = str_extract(name, "_\\d+"),
+         year = str_remove(year, "_"),
+         year = as.numeric(year),
+         name = str_remove(name, "_\\d+")) %>% 
+  mutate(rank = str_extract(name, "\\d+"),
+         rank = as.numeric(rank),
+         name = str_remove(name, "\\d+"),
+         name = str_trim(name))
+
+# Prepare for plot
+df.plot <- df %>% 
+  # find the numerator
+  group_by(name, rank, year, sdg) %>% 
+  summarise(n_keyword = sum(n_keyword)) %>% 
+  ungroup() %>% 
+  # find the denominator
+  group_by(name, rank, year) %>% 
+  mutate(n_keyword_total = sum(n_keyword)) %>% 
+  ungroup() %>% 
+  # find the ratio
+  mutate(ratio = n_keyword / n_keyword_total * 100) %>% 
+  mutate(ratio = round(ratio, 2)) %>% 
+  # find sdg number
+  mutate(sdg_number = str_extract(sdg, "\\d+"),
+         sdg_number = as.numeric(sdg_number)) %>% 
+  # set sdg as factor data
+  mutate(sdg = as_factor(sdg)) %>% 
+  mutate(sdg = fct_reorder(sdg, sdg_number))
+
+## Pick a company
+## TODO: input a company's rank
+AVAILABLE_COMPANIES <- df %>% select(name, rank) %>% distinct()
+company_rank <- 131
+company_name <- AVAILABLE_COMPANIES %>% filter(rank == company_rank) %>% pull(name)
+
+p1 <- df.plot %>% 
+  filter(rank == company_rank) %>% 
+  ggplot(aes(x = year, y = ratio, color = sdg)) +
+  geom_line()+
+  labs(x = "year", y = "percentage")
+
+p1
+
+## Save plot
+p1 %>% 
+  ggsave(filename = paste0("./data/result/fig_timetrend_across_SDGcate_", company_name, ".png"), 
+         device = "png",
+         dpi = 300, 
+         units = "in",
+         height = 12, width = 12)
