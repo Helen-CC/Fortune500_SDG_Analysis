34844dc0f57075f689b3ec3e4ad9273302205483
Helen-CC <bravo505202@hotmail.com>
2023-12-11 23:23:41 +0000

    Update 07_simple_reg.R

--- code/07_simple_reg.R
+++ code/07_simple_reg.R
@@ -7,7 +7,10 @@ library(tidyr)
 rm(list = rm()); gc()
 
 # TODO: choose the industry and SDG category you want manually
-NAICS2_CODES <- c(21, 31)
+# all industries in Excel: TM Final_FortuneG500 (2021)_v2
+# NAICS2_CODES <- c(11, 21, 22, 23, 31, 32, 33, 42, 44, 45, 48, 49, 51, 52, 53, 54, 55, 60, 62, 72)
+
+NAICS2_CODES <- c(11, 21, 22, 23, 31, 32, 33, 42, 44, 45, 48, 49, 51, 52, 53, 54, 55, 60, 62, 72)
 THE_SDG_CATEGORY <- "SDG13"
 
 # change the NAICS code 
